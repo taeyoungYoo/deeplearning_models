2ddcf3896b0006090013dc065fbb1e973ab88491
taeyoungYoo <52447154+taeyoungYoo@users.noreply.github.com>
2022-09-30 03:00:28 +0900

    baseline resnet50 structure updated

---
+++ ResNet/ResNet50.py
@@ -0,0 +1,17 @@
+'''
+    Implementation of ResNet-50 (https://arxiv.org/abs/1801.04381)
+'''
+
+import tensorflow as tf
+
+
+def identity_block():
+    None
+
+
+def convolutional_block():
+    None
+
+
+def ResNet50(input_shape = (224, 24, 3), classes = 1000):
+    None
